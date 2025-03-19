413b51dd38d2a678c8f3b0bc1ca57a3b463ba504
kook2468 <cdg9808@naver.com>
2025-03-19 17:34:46 +0900

    fix: CORS 설정 변경 - 허용 메소드 형식 배열로 변경

--- src/main.ts
+++ src/main.ts
@@ -13,9 +13,9 @@ async function bootstrap() {
       const app = await NestFactory.create(AppModule);
       app.enableCors({
         origin: process.env.FRONTEND_URL,
+        methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE', 'OPTIONS'],
+        allowedHeaders: ['Content-Type', 'Authorization'],
         credentials: true,
-        methods: 'GET,POST,PUT,PATCH,DELETE,OPTIONS',
-        allowedHeaders: 'Content-Type,Authorization',
       });
 
       app.use((req, res, next) => {
